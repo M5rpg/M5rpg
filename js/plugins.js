39b854221f2169e747ac8e8b3b6d580de0bb08fa
M5rpg <lovedonarudoryou@gmail.com>
2016-01-27 00:44:08 +0900

    a

--- js/plugins.js
+++ js/plugins.js
@@ -9,5 +9,5 @@ var $plugins =
 {"name":"WebAudioCache","status":true,"description":"A cache for WebAudio which keeps preloaded objects in memory while they are still recent or in use. Combined with TDDP Preload Manager, this should hopefully reduce BGM play lag to a minimum.","parameters":{}},
 {"name":"TDDP_PreloadManager","status":false,"description":"1.1.1 Preload resources on scene/map load as well as game startup for a smoother gameplay experience.          id:TDDP_PreloadManager","parameters":{"Preload On Map Load":"true","Preload System Music":"false","Preload System SFX":"false","Print Debug to Console":"false"}},
 {"name":"Cache Manager","status":true,"description":"Selectively clear the image cache for memory usage improvements","parameters":{"Mobile only":"Y","--------------------------------------------":"","Clear All":"Y","Clear Animations":"Y","Clear Battlebacks":"Y","Clear Battlers":"Y","Clear Characters":"Y","Clear Faces":"Y","Clear Parallaxes":"Y","Clear Pictures":"Y","Clear System":"N","Clear Tilesets":"Y","Clear Titles":"Y","Custom Images":""}},
-{"name":"liply_preload","status":true,"description":"テクスチャキャッシュの量を制限し、イベントで使用する画像を事前読み込みします。","parameters":{"Memory Limit":"50","Debug":"false"}}
+{"name":"liply_preload","status":false,"description":"テクスチャキャッシュの量を制限し、イベントで使用する画像を事前読み込みします。","parameters":{"Memory Limit":"50","Debug":"false"}}
 ];
