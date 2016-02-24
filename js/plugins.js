1e32586175c0f7514043317636734fbec339f6ec
M5rpg <lovedonarudoryou@gmail.com>
2016-02-24 20:13:35 +0900

    Bate

--- js/plugins.js
+++ js/plugins.js
@@ -19,5 +19,5 @@ var $plugins =
 {"name":"ItemBook","status":true,"description":"アイテム図鑑です。アイテムの詳細なステータスを表示します。","parameters":{"Unknown Data":"？？？？？？","Price Text":"価格","Equip Text":"装備","Type Text":"タイプ"}},
 {"name":"EnemyBook","status":true,"description":"モンスター図鑑です。敵キャラの詳細なステータスを表示します。","parameters":{}},
 {"name":"Torigoya_InputNamePrompt","status":false,"description":"名前入力ダイアログ機能を追加します","parameters":{"Max Length":"10","Message":"名前を入力してください","Maximum Message":"(%1 文字以内で入力)"}},
-{"name":"BootOpeningDemo","status":true,"description":"タイトル画面を出す前にオープニングデモから開始します","parameters":{"firstMapId":"1","startX":"0","startY":"0"}}
+{"name":"BootOpeningDemo","status":true,"description":"タイトル画面を出す前にオープニングデモから開始します","parameters":{"firstMapId":"50","startX":"0","startY":"0"}}
 ];
