c95359ae9104cad03e5c0e6b4eeb05878d8b209e
M5rpg <lovedonarudoryou@gmail.com>
2016-03-27 20:10:38 +0900

    2016.3.27.20.10

--- js/plugins/Title_Setting.js
+++ js/plugins/Title_Setting.js
@@ -11,25 +11,20 @@
 */
 
 (function() {
+	Scene_Title.prototype.drawGameTitle = function() {
+	    var x = 20;
+	    var y = Graphics.height / 4;
+	    var maxWidth = Graphics.width - x * 2;
+	    var text = '  ';
+	    this._gameTitleSprite.bitmap.outlineColor = 'black';
+	    this._gameTitleSprite.bitmap.outlineWidth = 8;
+	    this._gameTitleSprite.bitmap.fontSize = 72;
+	    this._gameTitleSprite.bitmap.drawText(text, x, y, maxWidth, 48, 'center');
+	};
 
-
-
-
-Scene_Title.prototype.drawGameTitle = function() {
-    var x = 20;
-    var y = Graphics.height / 4;
-    var maxWidth = Graphics.width - x * 2;
-    var text = '  ';
-    this._gameTitleSprite.bitmap.outlineColor = 'black';
-    this._gameTitleSprite.bitmap.outlineWidth = 8;
-    this._gameTitleSprite.bitmap.fontSize = 72;
-    this._gameTitleSprite.bitmap.drawText(text, x, y, maxWidth, 48, 'center');
-};
-
-
-Scene_Title.prototype.playTitleMusic = function() {
-    $gameSystem.replayBgm();
-    AudioManager.stopBgs();
-    AudioManager.stopMe();
-};
+	Scene_Title.prototype.playTitleMusic = function() {
+	    $gameSystem.replayBgm();
+	    AudioManager.stopBgs();
+	    AudioManager.stopMe();
+	};
 })();
